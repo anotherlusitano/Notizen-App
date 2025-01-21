e37a4b134f44a57d6bc271896b52785a5f25df73
João Ribeiro <joao.rosado.ribeiro@gmail.com>
2025-01-21 13:02:24 +0000

    Add simple design to Home Activity
    This is not finished yet

--- lib/activities/home_activity.dart
+++ lib/activities/home_activity.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:notizen/widgets/category_filter.dart';
+import 'package:notizen/widgets/todo_box.dart';
+
+import '../config/router.dart';
 
 class HomeActivity extends StatelessWidget {
   const HomeActivity({super.key});
@@ -6,14 +11,44 @@ class HomeActivity extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
+      appBar: AppBar(
+        title: Text("Tarefas"),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 10.0),
+            child: IconButton(
+              onPressed: () => context.goNamed(Routes.login.name),
+              icon: Icon(Icons.logout),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: ElevatedButton(
+        onPressed: () => (),
+        style: ButtonStyle(
+          shape: WidgetStatePropertyAll(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(5)),
+            ),
+          ),
+          backgroundColor: WidgetStatePropertyAll(Colors.lightBlue),
+        ),
+        child: Text(
+          "+",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 24,
+          ),
+        ),
+      ),
+      body: SafeArea(
         child: Center(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              Text(
-                "HOME",
-              ),
+              CategoryFilter(),
+              TodoBox(),
             ],
           ),
         ),
