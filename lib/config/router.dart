bbd3acaf6beee7d30d9fe480101fe7c67cc81c72
João Ribeiro <joao.rosado.ribeiro@gmail.com>
2025-01-24 16:26:27 +0000

    Add todo route

--- lib/config/router.dart
+++ lib/config/router.dart
@@ -2,8 +2,10 @@ import 'package:go_router/go_router.dart';
 import 'package:notizen/activities/home_activity.dart';
 import 'package:notizen/activities/login_activity.dart';
 import 'package:notizen/activities/sign_up_activity.dart';
+import 'package:notizen/activities/todo_activity.dart';
 
 enum Routes {
+  todo,
   home,
   login,
   signUp,
@@ -26,6 +28,11 @@ final router = GoRouter(
       name: Routes.home.name,
       path: '/home',
       builder: (context, state) => const HomeActivity(),
+    ),
+    GoRoute(
+      name: Routes.todo.name,
+      path: '/todo',
+      builder: (context, state) => TodoActivity(),
     ),
   ],
 );
