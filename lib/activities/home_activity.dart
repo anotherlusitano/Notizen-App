07f3059616317f94352e73ecc22d52783b5f7e2a
João Ribeiro <joao.rosado.ribeiro@gmail.com>
2025-01-24 10:51:51 +0000

    Show all Todos on the Home Activity

--- lib/activities/home_activity.dart
+++ lib/activities/home_activity.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:notizen/data/todo_repositoy.dart';
 import 'package:notizen/widgets/add_todo_button.dart';
 import 'package:notizen/widgets/category_filter.dart';
 import 'package:notizen/widgets/todo_box.dart';
@@ -32,7 +33,23 @@ class HomeActivity extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
               CategoryFilter(),
-              TodoBox(),
+              Expanded(
+                child: FutureBuilder(
+                    future: TodosRepository.instance.getTodos(),
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        return ListView.builder(
+                            itemCount: snapshot.data!.length,
+                            itemBuilder: (BuildContext context, int index) {
+                              final todo = snapshot.data![index];
+
+                              return TodoBox(todo: todo, index: index + 1);
+                            });
+                      } else {
+                        return SizedBox();
+                      }
+                    }),
+              ),
             ],
           ),
         ),
