4442b5900405ab0d39a9306f582cdf628abc53f3
João Ribeiro <joao.rosado.ribeiro@gmail.com>
2025-02-03 00:03:38 +0000

    Add wonderful design to Login Activity

--- lib/activities/login_activity.dart
+++ lib/activities/login_activity.dart
@@ -23,79 +23,108 @@ class _LoginActivityState extends State<LoginActivity> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromRGBO(253, 231, 172, 1),
+      bottomNavigationBar: TextButton(
+        onPressed: () => context.goNamed(Routes.signUp.name),
+        child: Text(
+          "Ainda não tens conta? Clica aqui!",
+          style: TextStyle(
+            color: Color.fromRGBO(159, 36, 30, 1),
+          ),
+        ),
+      ),
       body: SafeArea(
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "Bem-vindo!",
-                style: Theme.of(context).textTheme.displayLarge,
-              ),
-              SizedBox(height: 20.0),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  controller: _usernameController,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: "Insere o teu username...",
-                    hintStyle: Theme.of(context).textTheme.displaySmall,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  "assets/notizen.png",
+                  height: 200,
+                  width: 200,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    controller: _usernameController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: "Insere o teu username...",
+                      focusedBorder: OutlineInputBorder(),
+                      fillColor: Colors.white,
+                      filled: true,
+                      hintStyle: Theme.of(context).textTheme.displaySmall,
+                    ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  controller: _passwordController,
-                  obscureText: passwordIsHidden,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: "Insere a tua password...",
-                    hintStyle: Theme.of(context).textTheme.displaySmall,
-                    suffixIcon: InkWell(
-                      onTap: () => setState(() {
-                        passwordIsHidden = !passwordIsHidden;
-                      }),
-                      child: Icon(
-                        passwordIsHidden ? Icons.visibility_off_outlined : Icons.visibility_outlined,
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    controller: _passwordController,
+                    obscureText: passwordIsHidden,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white),
+                      ),
+                      focusedBorder: OutlineInputBorder(),
+                      fillColor: Colors.white,
+                      filled: true,
+                      hintText: "Insere a tua password...",
+                      hintStyle: Theme.of(context).textTheme.displaySmall,
+                      suffixIcon: InkWell(
+                        onTap: () => setState(() {
+                          passwordIsHidden = !passwordIsHidden;
+                        }),
+                        child: Icon(
+                          passwordIsHidden ? Icons.visibility_off_outlined : Icons.visibility_outlined,
+                          color: Color.fromRGBO(195, 108, 71, 1),
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-              ElevatedButton.icon(
-                onPressed: () async {
-                  final String password = _passwordController.text;
-                  final String username = _usernameController.text;
+                ElevatedButton.icon(
+                  onPressed: () async {
+                    final String password = _passwordController.text;
+                    final String username = _usernameController.text;
 
-                  if (password.trim().isEmpty || username.trim().isEmpty) return;
+                    if (password.trim().isEmpty || username.trim().isEmpty) return;
 
-                  final user = User(username, password, 0);
+                    final user = User(username, password, 0);
 
-                  final userFound = await UsersRepository.instance.loginLocalAccount(user);
+                    final userFound = await UsersRepository.instance.loginLocalAccount(user);
 
-                  if (userFound) {
+                    if (userFound) {
+                      if (!context.mounted) return;
+                      SnackMsg.showOk(context, 'Bem-vindo $username!');
+                      return context.goNamed(
+                        Routes.home.name,
+                        pathParameters: {'username': username},
+                      );
+                    }
+
                     if (!context.mounted) return;
-                    SnackMsg.showOk(context, 'Bem-vindo $username!');
-                    return context.goNamed(
-                      Routes.home.name,
-                      pathParameters: {'username': username},
-                    );
-                  }
-
-                  if (!context.mounted) return;
-                  SnackMsg.showError(context, 'Palavra-passe ou username errado!');
-                },
-                icon: Icon(Icons.login),
-                label: Text("Entrar"),
-              ),
-              SizedBox(height: 20.0),
-              TextButton(
-                onPressed: () => context.goNamed(Routes.signUp.name),
-                child: Text("Ainda não tens conta? Clica aqui!"),
-              ),
-            ],
+                    SnackMsg.showError(context, 'Palavra-passe ou username errado!');
+                  },
+                  icon: Icon(
+                    Icons.login,
+                    color: Color.fromRGBO(195, 108, 71, 1),
+                  ),
+                  label: Text(
+                    "Entrar",
+                    style: TextStyle(
+                      color: Color.fromRGBO(195, 108, 71, 1),
+                    ),
+                  ),
+                  style: ButtonStyle(
+                    backgroundColor: WidgetStateProperty.resolveWith(
+                      (states) => Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
